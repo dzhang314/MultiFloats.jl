28b2efdceaf904af7e86a58698383bda1d9e218b
David K. Zhang <dzhang314@gmail.com>
2025-04-03 05:15:34 -0700

    add mfadd and mfmul networks

--- src/MultiFloatsV3.jl
+++ src/MultiFloatsV3.jl
@@ -368,16 +368,21 @@ end
 end
 
 
+@inline function unsafe_fast_two_sum(x::T, y::T) where {T}
+    s = x + y
+    y_prime = s - x
+    e = y - y_prime
+    return (s, e)
+end
+
+
 @inline function fast_two_sum(x::T, y::T) where {T}
     @static if ENABLE_RUNTIME_ASSERTIONS
         if isfinite(x) & isfinite(y)
             @assert (iszero(x) | iszero(y)) || (exponent(x) >= exponent(y))
         end
     end
-    s = x + y
-    y_prime = s - x
-    e = y - y_prime
-    return (s, e)
+    return unsafe_fast_two_sum(x, y)
 end
 
 
@@ -389,7 +394,7 @@ end
     for _ = 1:N-1
         for i = N-1:-1:1
             push!(body, Expr(:(=), Expr(:tuple, xs[i], xs[i+1]),
-                Expr(:call, :fast_two_sum, xs[i], xs[i+1])))
+                Expr(:call, :unsafe_fast_two_sum, xs[i], xs[i+1])))
         end
     end
     push!(body, Expr(:return, Expr(:tuple, xs...)))
@@ -643,7 +648,7 @@ end
     (b, c) = fast_two_sum(b, c)
     (a, b) = fast_two_sum(a, b)
     @static if ENABLE_RUNTIME_ASSERTIONS
-        @assert (a, b) === two_sum(a, b)
+        @assert is_normalized((a, b))
     end
     return MultiFloat{T,2}((a, b))
 end
@@ -663,7 +668,7 @@ end
     (b, c) = two_sum(b, c)
     (a, b) = fast_two_sum(a, b)
     @static if ENABLE_RUNTIME_ASSERTIONS
-        @assert (a, b) === two_sum(a, b)
+        @assert is_normalized((a, b))
     end
     return MultiFloat{T,2}((a, b))
 end
@@ -693,10 +698,275 @@ end
     (c, d) = two_sum(c, d)
     (b, c) = fast_two_sum(b, c)
     @static if ENABLE_RUNTIME_ASSERTIONS
-        @assert (a, b) === two_sum(a, b)
-        @assert (b, c) === two_sum(b, c)
+        @assert is_normalized((a, b, c))
     end
     return MultiFloat{T,3}((a, b, c))
+end
+
+
+@inline function mfadd(
+    ::Val{2}, x::MultiFloat{T,3}, y::MultiFloat{T,4},
+) where {T}
+    @inbounds a = x._limbs[1]
+    @inbounds b = y._limbs[1]
+    @inbounds c = x._limbs[2]
+    @inbounds d = y._limbs[2]
+    @inbounds e = x._limbs[3]
+    @inbounds f = y._limbs[3]
+    @inbounds g = y._limbs[4]
+    # There are hundreds of roughly equivalent FPANs for (3, 4) -> 2 addition.
+    # This is a random choice from that pool; more principled analysis should
+    # be performed in the future.
+    (a, b) = two_sum(a, b)
+    (c, d) = two_sum(c, d)
+    (e, f) = two_sum(e, f)
+    (a, c) = fast_two_sum(a, c)
+    (d, e) = two_sum(d, e)
+    (f, g) = two_sum(f, g)
+    (a, d) = fast_two_sum(a, d)
+    (b, g) = fast_two_sum(b, g)
+    (c, f) = fast_two_sum(c, f)
+    (a, c) = fast_two_sum(a, c)
+    (d, e) = fast_two_sum(d, e)
+    (b, c) = two_sum(b, c)
+    (b, d) = two_sum(b, d)
+    (a, b) = fast_two_sum(a, b)
+    @static if ENABLE_RUNTIME_ASSERTIONS
+        @assert is_normalized((a, b))
+    end
+    return MultiFloat{T,2}((a, b))
+end
+
+
+################################################################################
+
+
+@inline function mfmul(
+    ::Val{2}, x::MultiFloat{T,1}, y::MultiFloat{T,2},
+) where {T}
+    @inbounds (a, b) = two_prod(x._limbs[1], y._limbs[1])
+    @inbounds c = x._limbs[1] * y._limbs[2]
+    (b, c) = two_sum(b, c)
+    (a, b) = fast_two_sum(a, b)
+    @static if ENABLE_RUNTIME_ASSERTIONS
+        @assert is_normalized((a, b))
+    end
+    return MultiFloat{T,2}((a, b))
+end
+
+
+@inline function mfmul(
+    ::Val{2}, x::MultiFloat{T,1}, y::MultiFloat{T,3},
+) where {T}
+    @inbounds (a, b) = two_prod(x._limbs[1], y._limbs[1])
+    @inbounds c = x._limbs[1] * y._limbs[2]
+    (b, c) = two_sum(b, c)
+    (a, b) = fast_two_sum(a, b)
+    @static if ENABLE_RUNTIME_ASSERTIONS
+        @assert is_normalized((a, b))
+    end
+    return MultiFloat{T,2}((a, b))
+end
+
+
+@inline function mfmul(
+    ::Val{2}, x::MultiFloat{T,2}, y::MultiFloat{T,1},
+) where {T}
+    @inbounds (a, b) = two_prod(x._limbs[1], y._limbs[1])
+    @inbounds c = x._limbs[2] * y._limbs[1]
+    (b, c) = two_sum(b, c)
+    (a, b) = fast_two_sum(a, b)
+    @static if ENABLE_RUNTIME_ASSERTIONS
+        @assert is_normalized((a, b))
+    end
+    return MultiFloat{T,2}((a, b))
+end
+
+
+@inline function mfmul(
+    ::Val{2}, x::MultiFloat{T,3}, y::MultiFloat{T,1},
+) where {T}
+    @inbounds (a, b) = two_prod(x._limbs[1], y._limbs[1])
+    @inbounds c = x._limbs[2] * y._limbs[1]
+    (b, c) = two_sum(b, c)
+    (a, b) = fast_two_sum(a, b)
+    @static if ENABLE_RUNTIME_ASSERTIONS
+        @assert is_normalized((a, b))
+    end
+    return MultiFloat{T,2}((a, b))
+end
+
+
+@inline function mfmul(
+    ::Val{2}, x::MultiFloat{T,2}, y::MultiFloat{T,2},
+) where {T}
+    @inbounds (a, b) = two_prod(x._limbs[1], y._limbs[1])
+    @inbounds c = x._limbs[1] * y._limbs[2]
+    @inbounds d = x._limbs[2] * y._limbs[1]
+    (c, d) = two_sum(c, d)
+    (b, c) = two_sum(b, c)
+    (a, b) = fast_two_sum(a, b)
+    @static if ENABLE_RUNTIME_ASSERTIONS
+        @assert is_normalized((a, b))
+    end
+    return MultiFloat{T,2}((a, b))
+end
+
+
+@inline function mfmul(
+    ::Val{2}, x::MultiFloat{T,3}, y::MultiFloat{T,2},
+) where {T}
+    @inbounds (a, b) = two_prod(x._limbs[1], y._limbs[1])
+    @inbounds c = x._limbs[1] * y._limbs[2]
+    @inbounds d = x._limbs[2] * y._limbs[1]
+    (c, d) = two_sum(c, d)
+    (b, c) = two_sum(b, c)
+    (a, b) = fast_two_sum(a, b)
+    @static if ENABLE_RUNTIME_ASSERTIONS
+        @assert is_normalized((a, b))
+    end
+    return MultiFloat{T,2}((a, b))
+end
+
+
+@inline function mfmul(
+    ::Val{3}, x::MultiFloat{T,3}, y::MultiFloat{T,3},
+) where {T}
+    @inbounds (a, b) = two_prod(x._limbs[1], y._limbs[1])
+    @inbounds (c, e) = two_prod(x._limbs[1], y._limbs[2])
+    @inbounds (d, f) = two_prod(x._limbs[2], y._limbs[1])
+    @inbounds g = x._limbs[1] * y._limbs[3]
+    @inbounds h = x._limbs[2] * y._limbs[2]
+    @inbounds i = x._limbs[3] * y._limbs[1]
+    # There are 24 roughly equivalent FPANs for (3, 3) -> 3 multiplication.
+    # They all have 12 TwoSum gates (depth 7) and have approximately 155-bit
+    # accuracy. Extensive testing suggests that is the most accurate by a
+    # very thin margin. Other candidates may have more gates that are
+    # reducible to FastTwoSum; this should be investigated in the future.
+    (c, d) = two_sum(c, d)
+    (e, f) = two_sum(e, f)
+    (g, i) = two_sum(g, i)
+    (b, c) = two_sum(b, c)
+    (g, h) = two_sum(g, h) # Interestingly, (e, h) also works here.
+    (a, b) = fast_two_sum(a, b)
+    (c, d) = fast_two_sum(c, d)
+    (e, g) = two_sum(e, g)
+    (c, e) = two_sum(c, e)
+    (b, c) = fast_two_sum(b, c)
+    (a, b) = fast_two_sum(a, b)
+    (b, c) = fast_two_sum(b, c)
+    @static if ENABLE_RUNTIME_ASSERTIONS
+        @assert is_normalized((a, b, c))
+    end
+    return MultiFloat{T,3}((a, b, c))
+end
+
+
+@inline function mfmul(
+    ::Val{4}, x::MultiFloat{T,2}, y::MultiFloat{T,2},
+) where {T}
+    @inbounds (a, b) = two_prod(x._limbs[1], y._limbs[1])
+    @inbounds (c, e) = two_prod(x._limbs[1], y._limbs[2])
+    @inbounds (d, f) = two_prod(x._limbs[2], y._limbs[1])
+    @inbounds (g, h) = two_prod(x._limbs[2], y._limbs[2])
+    (c, d) = two_sum(c, d)
+    (e, f) = two_sum(e, f)
+    (b, c) = two_sum(b, c)
+    (e, g) = two_sum(e, g)
+    (f, h) = fast_two_sum(f, h)
+    (a, b) = fast_two_sum(a, b)
+    (d, e) = fast_two_sum(d, e)
+    (f, g) = two_sum(f, g)
+    (c, d) = fast_two_sum(c, d)
+    (e, f) = fast_two_sum(e, f)
+    (b, c) = fast_two_sum(b, c)
+    (d, e) = fast_two_sum(d, e)
+    (a, b) = fast_two_sum(a, b)
+    (c, d) = fast_two_sum(c, d)
+    (b, c) = fast_two_sum(b, c)
+    (c, d) = fast_two_sum(c, d)
+    @static if ENABLE_RUNTIME_ASSERTIONS
+        @assert is_normalized((a, b, c, d))
+    end
+    return MultiFloat{T,4}((a, b, c, d))
+end
+
+
+@inline function mfmul(
+    ::Val{4}, x::MultiFloat{T,4}, y::MultiFloat{T,4},
+) where {T}
+    @inbounds (x1, x2) = two_prod(x._limbs[1], y._limbs[1])
+    @inbounds (x3, x5) = two_prod(x._limbs[1], y._limbs[2])
+    @inbounds (x4, x6) = two_prod(x._limbs[2], y._limbs[1])
+    @inbounds (x7, x10) = two_prod(x._limbs[1], y._limbs[3])
+    @inbounds (x8, x11) = two_prod(x._limbs[2], y._limbs[2])
+    @inbounds (x9, x12) = two_prod(x._limbs[3], y._limbs[1])
+    @inbounds x13 = x._limbs[1] * y._limbs[4]
+    @inbounds x14 = x._limbs[2] * y._limbs[3]
+    @inbounds x15 = x._limbs[3] * y._limbs[2]
+    @inbounds x16 = x._limbs[4] * y._limbs[1]
+    # The following FPAN for (4, 4) -> 4 multiplication is conjectural.
+    # Despite several days of computational effort, the combinatorial search
+    # for (4, 4) -> 4 multiplication networks has not yet converged.
+    x3, x4 = two_sum(x3, x4)
+    x5, x6 = two_sum(x5, x6)
+    x7, x9 = two_sum(x7, x9)
+    x10 += x12
+    x13 += x16
+    x14 += x15
+    x2, x3 = two_sum(x2, x3)
+    x5, x7 = two_sum(x5, x7)
+    x6, x9 = two_sum(x6, x9)
+    x10, x13 = two_sum(x10, x13)
+    x1, x2 = fast_two_sum(x1, x2)
+    x3, x8 = fast_two_sum(x3, x8)
+    x4, x5 = two_sum(x4, x5)
+    x6, x10 = two_sum(x6, x10)
+    x7, x11 = two_sum(x7, x11)
+    x3, x5 = two_sum(x3, x5)
+    x7, x14 = two_sum(x7, x14)
+    x3, x4 = two_sum(x3, x4)
+    x5, x8 = two_sum(x5, x8)
+    x6, x7 = two_sum(x6, x7)
+    x2, x3 = two_sum(x2, x3)
+    x4, x5 = two_sum(x4, x5)
+    x1, x2 = fast_two_sum(x1, x2)
+    x4, x6 = two_sum(x4, x6)
+    x3, x4 = two_sum(x3, x4)
+    x2, x3 = fast_two_sum(x2, x3)
+    x3, x4 = fast_two_sum(x3, x4)
+    @static if ENABLE_RUNTIME_ASSERTIONS
+        @assert is_normalized((x1, x2, x3, x4))
+    end
+    return MultiFloat{T,4}((x1, x2, x3, x4))
+end
+
+
+@inline function mfmul(
+    ::Val{4}, x::MultiFloat{T,2}, y::MultiFloat{T,3},
+) where {T}
+    # This is a stopgap solution until an optimized
+    # (2, 3) -> 4 multiplication network is found.
+    _zero = zero(T)
+    @inbounds x_padded = MultiFloat{T,4}(
+        (x._limbs[1], x._limbs[2], _zero, _zero))
+    @inbounds y_padded = MultiFloat{T,4}(
+        (y._limbs[1], y._limbs[2], y._limbs[3], _zero))
+    return mfmul(Val{4}(), x_padded, y_padded)
+end
+
+
+@inline function mfmul(
+    ::Val{4}, x::MultiFloat{T,3}, y::MultiFloat{T,2},
+) where {T}
+    # This is a stopgap solution until an optimized
+    # (3, 2) -> 4 multiplication network is found.
+    _zero = zero(T)
+    @inbounds x_padded = MultiFloat{T,4}(
+        (x._limbs[1], x._limbs[2], x._limbs[3], _zero))
+    @inbounds y_padded = MultiFloat{T,4}(
+        (y._limbs[1], y._limbs[2], _zero, _zero))
+    return mfmul(Val{4}(), x_padded, y_padded)
 end
 
 
