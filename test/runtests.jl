e511d0b1c1d877e99f3750b3cd8a74e71ae63805
David K. Zhang <dzhang314@gmail.com>
2026-01-24 01:42:39 -0800

    remove obsolete precise types from test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,20 +2,16 @@ using MultiFloats
 using Test: @test
 
 for T in [
+    Float32x1, Float32x2, Float32x3, Float32x4,
     Float64x1, Float64x2, Float64x3, Float64x4,
-    PreciseFloat64x1, PreciseFloat64x2, PreciseFloat64x3, PreciseFloat64x4,
+    Vec1Float32x1, Vec1Float32x2, Vec1Float32x3, Vec1Float32x4,
     Vec1Float64x1, Vec1Float64x2, Vec1Float64x3, Vec1Float64x4,
+    Vec2Float32x1, Vec2Float32x2, Vec2Float32x3, Vec2Float32x4,
     Vec2Float64x1, Vec2Float64x2, Vec2Float64x3, Vec2Float64x4,
+    Vec4Float32x1, Vec4Float32x2, Vec4Float32x3, Vec4Float32x4,
     Vec4Float64x1, Vec4Float64x2, Vec4Float64x3, Vec4Float64x4,
+    Vec8Float32x1, Vec8Float32x2, Vec8Float32x3, Vec8Float32x4,
     Vec8Float64x1, Vec8Float64x2, Vec8Float64x3, Vec8Float64x4,
-    Vec1PreciseFloat64x1, Vec1PreciseFloat64x2,
-    Vec1PreciseFloat64x3, Vec1PreciseFloat64x4,
-    Vec2PreciseFloat64x1, Vec2PreciseFloat64x2,
-    Vec2PreciseFloat64x3, Vec2PreciseFloat64x4,
-    Vec4PreciseFloat64x1, Vec4PreciseFloat64x2,
-    Vec4PreciseFloat64x3, Vec4PreciseFloat64x4,
-    Vec8PreciseFloat64x1, Vec8PreciseFloat64x2,
-    Vec8PreciseFloat64x3, Vec8PreciseFloat64x4,
 ]
 
     # All MultiFloat types should be bits types.
